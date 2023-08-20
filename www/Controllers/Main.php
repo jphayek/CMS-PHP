4243cf141c6277e1a5d567df00eb79b5f6535038
Jp Hayek <jphayek1@gmail.com>
2023-08-20 16:20:51 +0200

    Main

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -9,24 +9,7 @@ use App\datatable\articleTableFront;
 
 class Main
 {
-   /* public function home(): void
-    {
-        $pseudo = "Prof";
-        $view = new View("Main/home", "front");
-        $view->assign("pseudo", $pseudo);
-        $view->assign("age", 30);
-        $view->assign("titleseo", "supernouvellepage");
-    }*/
 
-    public function contact(): void
-    {
-        echo "Page de contact";
-    }
-
-    public function aboutUs(): void
-    {
-        echo "Page à propos";
-    }
 
     public function home()
     {
